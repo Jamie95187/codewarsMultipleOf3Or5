900bb69d4ef315d12d7ccb6fdc5e617cc89cef69
Jamie95187 <jamiewong63@googlemail.com>
2020-04-07 04:40:47 +0100

    test: input = 10

--- src/codewarsMultipleOf3Or5/MultipleOf3Or5.java
+++ src/codewarsMultipleOf3Or5/MultipleOf3Or5.java
@@ -3,7 +3,9 @@ package codewarsMultipleOf3Or5;
 public class MultipleOf3Or5 {
 	
 	public static int sumOfMultiples(int n) {
-		if (n == 7) {
+		if (n == 10) {
+			return 3 + 5 + 6 + 9;
+		} else if (n == 7) {
 			return 3 + 5 + 6;
 		} else if (n == 6) {
 			return 3 + 5;
--- src/codewarsMultipleOf3Or5/solutionTest.java
+++ src/codewarsMultipleOf3Or5/solutionTest.java
@@ -12,13 +12,17 @@ public class solutionTest {
 	}
 
 	@Test
-	public void sumOfMultiples_6_return3() {
+	public void sumOfMultiples_6_return8() {
 		assertEquals(8, MultipleOf3Or5.sumOfMultiples(6));
 	}
 	
 	@Test
-	public void sumOfMultiples_7_return3() {
+	public void sumOfMultiples_7_retur14() {
 		assertEquals(14, MultipleOf3Or5.sumOfMultiples(7));
 	}
 	
+	@Test
+	public void sumOfMultiples_10_return23() {
+		assertEquals(23, MultipleOf3Or5.sumOfMultiples(10));
+	}
 }
