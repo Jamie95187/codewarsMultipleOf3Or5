8e31cbfbf52d7ecd604cc016b8005e35c3d3a1d4
Jamie95187 <jamiewong63@googlemail.com>
2020-04-07 04:30:30 +0100

    test: input = 4

---
+++ src/codewarsMultipleOf3Or5/MultipleOf3Or5.java
@@ -0,0 +1,9 @@
+package codewarsMultipleOf3Or5;
+
+public class MultipleOf3Or5 {
+	
+	public static int sumOfMultiples(int n) {
+		return 3;
+	}
+	
+}
--- src/codewarsMultipleOf3Or5/solutionTest.java
+++ src/codewarsMultipleOf3Or5/solutionTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class solutionTest {
-
+	
+	private static MultipleOf3Or5 test = new MultipleOf3Or5(); 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void sumOfMultiples_4_return3() {
+		assertEquals(3, test.sumOfMultiples(4));
 	}
 
 }
